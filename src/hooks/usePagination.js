f0e846b9cacb183c0141c3f840c44ca82aea8b25
Faqih Muntashir <itsfaqih@gmail.com>
2020-10-09 23:20:37 +0700

    Refactored pagination, changed maxPageNumber to variable instead of state

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,18 +1,15 @@
 import { useEffect, useState } from 'react';
 
-export default function usePagination(initialData, perPage = 6) {
-  const [data, setData] = useState([]);
+export default function usePagination(initialData = [], perPage = 6) {
+  const [data, setData] = useState(initialData);
   const [currentPage, setCurrentPage] = useState(1);
-  const [maxPageNumber, setMaxPageNumber] = useState(null);
 
+  const maxPageNumber = Math.ceil(initialData.length / perPage);
   const nextPageNumber = currentPage === maxPageNumber ? null : currentPage + 1;
   const previousPageNumber = currentPage === 1 ? null : currentPage - 1;
 
   useEffect(() => {
-    if (initialData) {
-      setData(initialData.slice((currentPage - 1) * perPage, currentPage * perPage));
-      setMaxPageNumber(Math.ceil(initialData.length / perPage));
-    }
+    setData(initialData.slice((currentPage - 1) * perPage, currentPage * perPage));
   }, [initialData, perPage, currentPage]);
 
   const nextPage = () => {
